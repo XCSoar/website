9df55d901e985344d6292c0c172f364731543463
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2011-12-24 17:38:31 +0100

    download/waypoints: Added waypoint file list below the map widget

--- download/waypoints/waypoints.js
+++ download/waypoints/waypoints.js
@@ -12,17 +12,40 @@ google.maps.event.addListener(map, 'click', function() {
     if (info) info.close();
 });
 
-for (var name in WAYPOINTS) {
-    var file_info = WAYPOINTS[name];
+function compare(a, b)
+{
+	a = a[0].toLowerCase();
+	a = a.replace(/ä/g,"a");
+	a = a.replace(/ö/g,"o");
+	a = a.replace(/ü/g,"u");
+	a = a.replace(/ß/g,"s");
+
+	b = b[0].toLowerCase();
+	b = b.replace(/ä/g,"a");
+	b = b.replace(/ö/g,"o");
+	b = b.replace(/ü/g,"u");
+	b = b.replace(/ß/g,"s");
+
+	return (a == b) ? 0 : ((a > b) ? 1 : -1);
+}
+
+var sorted_waypoints = [];
+for (var name in WAYPOINTS)
+    sorted_waypoints.push([name, WAYPOINTS[name]]);
+
+sorted_waypoints.sort(compare);
+
+for (var country in sorted_waypoints) {
+    var file_info = sorted_waypoints[country][1];
     
     var latlon = new google.maps.LatLng(file_info['average'][0], file_info['average'][1]);
     var marker = new google.maps.Marker({
         position: latlon, 
         map: map, 
-        title: name
+        title: sorted_waypoints[country][0]
     });
 
-    marker.country = name;
+    marker.country = sorted_waypoints[country][0];
     marker.waypoints = file_info['size'];
  
     google.maps.event.addListener(marker, 'click', function() {
@@ -38,4 +61,9 @@ for (var name in WAYPOINTS) {
         });        
         info.open(map, this);   
     });
+
+    var map_list_info = "<hr/><h2>" + sorted_waypoints[country][0] + "</h2><br/><a href=\"http://download.xcsoar.org/waypoints/" + 
+                        sorted_waypoints[country][0] + ".cup\" type='application/octet-stream'>Download</a> (" + file_info['size'] + " waypoints)<br/><br/>";
+
+    $("#wp_list").append(map_list_info);
 }
