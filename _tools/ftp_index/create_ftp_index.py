4371f0917f1e76f451c46baf0f11cf40ea3195c7
Tobias Bieniek <Tobias.Bieniek@gmx.de>
2013-02-23 01:16:30 +0100

    ftp_index: Fixed PEP8 issues

--- _tools/ftp_index/create_ftp_index.py
+++ _tools/ftp_index/create_ftp_index.py
@@ -1,28 +1,31 @@
-import os
 import sys
 import ftplib
 import re
 import datetime
 import math
 
+
 def filesizeformat(bytes, precision=2):
     """Returns a humanized string for a given amount of bytes"""
     bytes = int(bytes)
     if bytes is 0:
         return '0 bytes'
-    
+
     log = math.floor(math.log(bytes, 1024))
     return "%.*f %s" % (precision,
                        bytes / math.pow(1024, log),
                        ['bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
                        [int(log)])
-def sort_nicely(l): 
-    """ Sort the given list in the way that humans expect. 
-    """ 
-    convert = lambda text: int(text) if text.isdigit() else text 
-    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key[0]) ] 
-    l.sort( key=alphanum_key )
-    
+
+
+def sort_nicely(l):
+    """ Sort the given list in the way that humans expect.
+    """
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key[0])]
+    l.sort(key=alphanum_key)
+
+
 def read_date_time(str):
     year = int(str[:4])
     month = int(str[4:6])
@@ -30,20 +33,21 @@ def read_date_time(str):
     hour = int(str[8:10])
     minute = int(str[10:12])
     second = int(str[12:14])
-    
+
     return datetime.datetime(year, month, day, hour, minute, second)
-    
+
+
 def get_folder_content(ftp):
     ftp_list = []
     ftp.retrlines('MLSD', ftp_list.append)
     #dir(ftp_list.append)
-    
+
     subdirs = []
-    files = []    
+    files = []
     for item in ftp_list:
         item = item.split(';')
-        name = item[-1].strip()  
-        last_mod = read_date_time(item[2].replace("modify=", ""))     
+        name = item[-1].strip()
+        last_mod = read_date_time(item[2].replace("modify=", ""))
         size = int(item[1].replace("size=", "").replace("sizd=", ""))
 
         if item[0] == "type=file":
@@ -53,20 +57,22 @@ def get_folder_content(ftp):
             if name != ".." and name != ".":
                 subdirs.append([name, last_mod])
 
-    sort_nicely(subdirs)        
-    sort_nicely(files)        
+    sort_nicely(subdirs)
+    sort_nicely(files)
 
     return subdirs, files
+
 
 def get_readme(ftp, path, files):
     readme_file = None
     for file in files:
-        if file[0].upper().startswith("README"): 
+        if file[0].upper().startswith("README"):
             readme_file = file[0]
             break
 
-    if readme_file == None: return ""
-    
+    if readme_file == None:
+        return ""
+
     f = open("readme.tpl", "r")
     template = f.read()
     f.close()
@@ -77,6 +83,7 @@ def get_readme(ftp, path, files):
 
     return template.replace("%%filename%%", readme_file).replace("%%content%%", content)
 
+
 def create_index_file(ftp, path, subdirs, files):
     f = open("index.tpl", "r")
     index_template = f.read()
@@ -85,41 +92,43 @@ def create_index_file(ftp, path, subdirs, files):
     f = open("subdir.tpl", "r")
     subdir_template = f.read()
     f.close()
-    
+
     f = open("file.tpl", "r")
     file_template = f.read()
     f.close()
-    
+
     content_subdirs = ""
     for dir in subdirs:
         content_subdirs += subdir_template.replace("%%path%%", path + dir[0] + "/").replace("%%title%%", dir[0])
-    
+
     content_files = ""
     for file in files:
         content_files += file_template.replace("%%path%%", path + file[0]).replace("%%size%%", filesizeformat(file[2])).replace("%%title%%", file[0])
-    
+
     sep = "" if len(files) == 0 or len(subdirs) == 0 else "<hr/>"
-    
+
     path_parts = path.split("/")
     path2 = "/"
     path_str = "/ "
     for part in path_parts:
         if part != "":
-            path2 += part + "/" 
+            path2 += part + "/"
             path_str += "<a href=\"" + path2 + "\">" + part + "</a> / "
-    
+
     readme = get_readme(ftp, path, files)
     html = index_template.replace("%%path%%", path_str).replace("%%sep%%", sep).replace("%%content_files%%", content_files).replace("%%content_subdirs%%", content_subdirs).replace("%%readme%%", readme)
-    
+
     f = open("index.html", "w")
     f.write(html)
     f.close()
-  
+
+
 def upload_index_file(ftp):
     file = open("index.html", 'rb')
     ftp.storbinary('STOR index.html', file)
     file.close()
-    
+
+
 def create_folder_index(path, ftp, recursive):
     print "Entering folder " + path
     ftp.cwd(path)
@@ -130,7 +139,7 @@ def create_folder_index(path, ftp, recursive):
     create_index_file(ftp, path, subdirs, files)
     print "Uploading index for folder " + path
     upload_index_file(ftp)
-    
+
     if recursive:
         for dir in subdirs:
             create_folder_index(path + dir[0] + "/", ftp, recursive)
@@ -146,9 +155,9 @@ def main():
     if len(sys.argv) < 2:
         help()
         return
-    
+
     password = sys.argv[1]
-    
+
     recursive = False
     ftp_dir = '/releases'
     for arg in sys.argv[2:]:
@@ -156,12 +165,12 @@ def main():
             recursive = True
         else:
             ftp_dir = arg.rstrip('/')
-    
+
     print "Connecting to server ..."
-    ftp = ftplib.FTP('download.xcsoar.org', 'xcsoar@ddbits.com', password) # Connect
-    
+    ftp = ftplib.FTP('download.xcsoar.org', 'xcsoar@ddbits.com', password)
+
     create_folder_index(ftp_dir + '/', ftp, recursive)
-    
+
     ftp.quit()
-    
+
 main()
--- _tools/ftp_index/create_local_index.py
+++ _tools/ftp_index/create_local_index.py
@@ -5,27 +5,30 @@ import re
 from datetime import datetime
 import math
 
+
 def filesizeformat(bytes, precision=2):
     """Returns a humanized string for a given amount of bytes"""
     bytes = int(bytes)
     if bytes is 0:
         return '0 bytes'
-    
+
     log = math.floor(math.log(bytes, 1024))
     return "%.*f %s" % (precision,
                        bytes / math.pow(1024, log),
                        ['bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
                        [int(log)])
-def sort_nicely(l): 
-    """ Sort the given list in the way that humans expect. 
-    """ 
-    convert = lambda text: int(text) if text.isdigit() else text 
-    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key[0]) ] 
-    l.sort( key=alphanum_key )
-    
+
+
+def sort_nicely(l):
+    """Sort the given list in the way that humans expect."""
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key[0])]
+    l.sort(key=alphanum_key)
+
+
 def get_folder_content(path):
     subdirs = []
-    files = []    
+    files = []
     for name in os.listdir(path):
         filepath = os.path.join(path, name)
         last_mod = datetime.fromtimestamp(os.path.getmtime(filepath))
@@ -38,20 +41,22 @@ def get_folder_content(path):
             if name != ".." and name != ".":
                 subdirs.append([name, last_mod])
 
-    sort_nicely(subdirs)        
-    sort_nicely(files)        
+    sort_nicely(subdirs)
+    sort_nicely(files)
 
     return subdirs, files
+
 
 def get_readme(path, files):
     readme_file = None
     for file in files:
-        if file[0].upper().startswith("README"): 
+        if file[0].upper().startswith("README"):
             readme_file = file[0]
             break
 
-    if readme_file == None: return ""
-    
+    if readme_file == None:
+        return ""
+
     f = open("readme.tpl", "r")
     template = f.read()
     f.close()
@@ -62,6 +67,7 @@ def get_readme(path, files):
 
     return template.replace("%%filename%%", readme_file).replace("%%content%%", content)
 
+
 def create_index_file(path, subdirs, files):
     f = open("index.tpl", "r")
     index_template = f.read()
@@ -70,42 +76,43 @@ def create_index_file(path, subdirs, files):
     f = open("subdir.tpl", "r")
     subdir_template = f.read()
     f.close()
-    
+
     f = open("file.tpl", "r")
     file_template = f.read()
     f.close()
-    
+
     content_subdirs = ""
     for dir in subdirs:
         content_subdirs += subdir_template.replace("%%path%%", path + dir[0] + "/").replace("%%title%%", dir[0])
-    
+
     content_files = ""
     for file in files:
         content_files += file_template.replace("%%path%%", path + file[0]).replace("%%size%%", filesizeformat(file[2])).replace("%%title%%", file[0])
-    
+
     sep = "" if len(files) == 0 or len(subdirs) == 0 else "<hr/>"
-    
+
     path_parts = path.split("/")
     path2 = "/"
     path_str = "/ "
     for part in path_parts:
         if part != "":
-            path2 += part + "/" 
+            path2 += part + "/"
             path_str += "<a href=\"" + path2 + "\">" + part + "</a> / "
-    
+
     readme = get_readme(path, files)
     html = index_template.replace("%%path%%", path_str).replace("%%sep%%", sep).replace("%%content_files%%", content_files).replace("%%content_subdirs%%", content_subdirs).replace("%%readme%%", readme)
-    
+
     f = open(os.path.join(path, "index.html"), "w")
     f.write(html)
     f.close()
-  
+
+
 def create_folder_index(path, recursive):
     print "Listing folder " + path
     subdirs, files = get_folder_content(path)
     print "Creating index for folder " + path
     create_index_file(path, subdirs, files)
-    
+
     if recursive:
         for dir in subdirs:
             create_folder_index(path + dir[0] + "/", recursive)
