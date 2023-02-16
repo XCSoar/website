c242570953763be8d6c6108a463416aecc726056
Philipp Wollschlegel <folken@kabelsalat.ch>
2023-02-16 17:39:06 +0100

    fabfile: fix quoting, indents

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,12 @@
+# -*- coding: utf-8 -*-
 from fabric.api import local
 
 
 def render():
-    """ Renders the website to the _site folder """
-    local('jekyll')
+    """Renders the website to the _site folder"""
+    local("jekyll")
 
 
 def serve():
-    """ Runs a local server on port 8080 that serves the website """
-    local('jekyll --server 8080 --auto')
+    """Runs a local server on port 8080 that serves the website"""
+    local("jekyll --server 8080 --auto")
